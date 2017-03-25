090a36867818a41ea5ebdd172294c1dae76b1f99
Michael Constantin <constantinm@sharplabs.com>
2017-03-25 16:49:12 -0700

    fix static variables

--- booksProject/settings.py
+++ booksProject/settings.py
@@ -135,10 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'books', 'static', 'images')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'staticfiles')
+STATIC_DIR = STATIC_ROOT = os.path.join(BASE_DIR, 'books', 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Simplified static file serving.
